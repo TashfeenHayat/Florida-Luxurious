50971647766f03b97e2748655fe8841a984b606a
Abraham Peter <abraham.peter.hash@gmail.com>
2024-05-30 18:27:19 +0500

    working on the on scroll animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import LoginAdmin from "./pages/Admin/LoginAdmin";
 import Home from "./pages/Home/Home";
 import Signup from "./pages/Signup";
@@ -28,9 +28,17 @@ import AgentListing from "./pages/AgentListing";
 import AgentSold from "./pages/AgentSold";
 import Communities from "./pages/Communities";
 import Mls from "./pages/Mls";
+import AOS from "aos";
+import "aos/dist/aos.css";
 function App() {
   const location = useLocation();
   let isAdminRoute = location.pathname.startsWith("/admin");
+
+  useEffect(() => {
+    AOS.init({
+      once: false, // whether animation should happen only once
+    });
+  }, []);
 
   return (
     <>
--- src/pages/Home/FloridaProperties.jsx
+++ src/pages/Home/FloridaProperties.jsx
@@ -20,14 +20,34 @@ function FloridaProperties() {
             style={{ position: "sticky", top: "0" }}
           >
             <div>
-              <Title level={1} className="text-white">
+              <Title level={1} className="text-white" data-aos="fade-right">
                 Florida Luxurious Properties
               </Title>
               <Flex gap={20} justify="center" wrap="wrap">
-                <img src={Bagde} />
-                <img src={Bagde2} />
-                <img src={Bagde3} />
-                <img src={Bagde4} />
+                <img
+                  src={Bagde}
+                  data-aos="fade-right"
+                  data-aos-easing="ease-in-sine"
+                  data-aos-duration="1000"
+                />
+                <img
+                  src={Bagde2}
+                  data-aos="fade-right"
+                  data-aos-easing="ease-in-sine"
+                  data-aos-duration="1500"
+                />
+                <img
+                  src={Bagde3}
+                  data-aos="fade-right"
+                  data-aos-easing="ease-in-sine"
+                  data-aos-duration="2000"
+                />
+                <img
+                  src={Bagde4}
+                  data-aos="fade-right"
+                  data-aos-easing="ease-in-sine"
+                  data-aos-duration="2500"
+                />
               </Flex>
             </div>
           </Flex>
@@ -48,22 +68,37 @@ function FloridaProperties() {
                 className="text-white f-64 mt-150 text-center"
                 level={1}
                 style={{ marginBottom: "0px" }}
+                data-aos="fade-left"
+                data-aos-duration="1500"
               >
                 #1
                 <br />{" "}
-                <Text className="text-gray text-upper f-16 mt-16">
+                <Text
+                  className="text-gray text-upper f-16 mt-16"
+                  data-aos="fade-left"
+                >
                   Ranked Private Brokerage
                 </Text>
               </Title>
 
-              <Title className="text-white f-64 text-center" level={1}>
+              <Title
+                className="text-white f-64 text-center"
+                level={1}
+                data-aos="fade-left"
+                data-aos-duration="1500"
+              >
                 $1.8 BILLION <br />
-                <Text className="text-gray text-upper f-16 ">
+                <Text className="text-gray text-upper f-16">
                   Has exceeded the mark
                 </Text>
               </Title>
 
-              <Title className="text-white f-64 text-center" level={1}>
+              <Title
+                className="text-white f-64 text-center"
+                level={1}
+                data-aos="fade-left"
+                data-aos-duration="1500"
+              >
                 $1,831,197,188 <br />{" "}
                 <Text className="text-gray text-upper f-16 ">Total Sales</Text>
               </Title>
