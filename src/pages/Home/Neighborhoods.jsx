837278fdb25eafd241f79f779bae5bdf22c163e3
Abraham Peter <abraham.peter.hash@gmail.com>
2024-06-06 03:10:30 +0500

    new deisgn feature neighborhoods

--- src/pages/Home/Neighborhoods.jsx
+++ src/pages/Home/Neighborhoods.jsx
@@ -1,9 +1,14 @@
-import React from "react";
-import { Typography, Flex } from "antd";
+import React, { useState } from "react";
+import { Typography, Flex, Row, Col } from "antd";
 import Slider from "react-slick";
 import Florida from "../../assets/florida.png";
+import useCommunities from "../../hooks/useCommunities";
+import { useNavigate } from "react-router-dom";
 const { Title, Text, Paragraph } = Typography;
 function Neighborhoods() {
+  const { data, isLoading } = useCommunities();
+  const navigate = useNavigate();
+
   const settings = {
     className: "center",
     infinite: true,
@@ -48,7 +53,42 @@ function Neighborhoods() {
       <Title className="florida-heading-feature-negibour" level={1}>
         Feature Neighborhoods
       </Title>
-      <Flex
+      <Row className="px-4">
+        {data?.filters?.map((community, index) => (
+          <Col lg={6} md={12} sm={24} key={index}>
+            <div className="displayy-teamimg-center show-btn-community-home">
+              <img src={Florida} width="100%" className="img-op" />
+              <div
+                style={{
+                  position: "absolute",
+                  top: "50%",
+                  left: "0%",
+                  width: "100%",
+                }}
+              >
+                <Flex
+                  justify={"center"}
+                  align={"center"}
+                  style={{ width: "95%" }}
+                  vertical
+                >
+                  <Text className="text-upper text-white f-24 f-100">
+                    {community?.name}
+                  </Text>
+
+                  <button
+                    className="button-view1"
+                    onClick={() => navigate(`/community/${community?._id}`)}
+                  >
+                    View All
+                  </button>
+                </Flex>
+              </div>
+            </div>
+          </Col>
+        ))}
+      </Row>
+      {/* <Flex
         justify={"center"}
         align={"center"}
         style={{
@@ -260,7 +300,7 @@ function Neighborhoods() {
       </Flex>
       <Flex justify="center" align="center">
         <button className="button-neighborhood">See All Neighborhoods </button>
-      </Flex>
+      </Flex> */}
     </div>
   );
 }
