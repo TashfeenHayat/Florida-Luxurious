132b2c0a3f21d30cf26abfa8b576cae92e58b496
A-hasan-code <alihasan12soft786@gmail.com>
2024-08-05 22:08:36 +0500

    "Home page  responsive"

--- src/pages/SearchCommunity/SearchCommunity.jsx
+++ src/pages/SearchCommunity/SearchCommunity.jsx
@@ -1,15 +1,15 @@
 import React, { useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { IoLocationOutline, IoPricetagOutline } from "react-icons/io5";
-
 import BackgroundImage from "../../components/BackgroundImage";
-import { Typography, Row, Col, Flex, Pagination, Spin, Image } from "antd";
+import { Typography, Row, Col, Pagination, Spin, Image } from "antd";
 import FeaturedPropertiesImage from "../../assets/Agent.png";
 import Icons from "../../components/Icons";
 import { Container } from "react-bootstrap";
 import useProperties from "../../hooks/useProperties";
 
 const { Title, Text } = Typography;
+
 function useQuery() {
   return new URLSearchParams(useLocation().search);
 }
@@ -18,7 +18,6 @@ function SearchCommunity() {
   const [currentPage, setCurrentPage] = useState(1);
   const itemsPerPage = 6;
   let query = useQuery();
-  console.log(query.get("name"));
   const { data, isLoading } = useProperties(
     null,
     itemsPerPage,
@@ -27,9 +26,11 @@ function SearchCommunity() {
     null,
     query.get("name")
   );
+
   const handlePageChange = (page) => {
     setCurrentPage(page);
   };
+
   const navigate = useNavigate();
 
   return (
@@ -45,17 +46,17 @@ function SearchCommunity() {
         <Container className="pt-98 pb-98">
           <Title level={3}>Search Result = {data?.totalCount}</Title>
 
-          <Row gutter={[60, 60]}>
+          <Row gutter={[16, 16]}>
             {data?.properties?.map((property, index) => (
               <Col
-                lg={12}
-                md={12}
-                sm={24}
+                lg={8}  // 3 columns on large screens
+                md={12}  // 2 columns on medium screens
+                sm={24}  // 1 column on small screens
                 key={index}
                 onClick={() => navigate(`/features/${property?._id}`)}
                 style={{ cursor: "pointer" }}
               >
-                <div className="displayy-teamimg-center">
+                <div className="property-card">
                   <Image
                     src={
                       property?.media?.[0]?.mdUrl ||
@@ -66,54 +67,44 @@ function SearchCommunity() {
                     fallback="https://placehold.co/618x489"
                     preview={false}
                   />
-                  <div className="more-info-property">
+                  <div className="property-details">
                     <Flex
-                      vertical
-                      align={"center"}
+                      direction="vertical"
+                      align="center"
                       justify="center"
                       style={{ height: "100%" }}
-                      gap={30}
+                      gap={16}
                     >
-                      <Flex vertical>
-                        <Text className="text-center text-upper f-24 f-bold text-white">
-                          address
+                      <Flex direction="vertical">
+                        <Text className="text-center text-upper f-16 f-bold text-white">
+                          Address
                         </Text>
-                        <Text className="text-center text-upper f-24 f-100 text-gray">
+                        <Text className="text-center text-upper f-16 f-100 text-gray">
                           {property?.addressLine1} {property?.addressLine2}
                         </Text>
                       </Flex>
-                      <Flex vertical>
-                        <Text className="text-center text-upper f-24 f-bold text-white">
-                          last list price
+                      <Flex direction="vertical">
+                        <Text className="text-center text-upper f-16 f-bold text-white">
+                          Last List Price
                         </Text>
-                        <Text className="text-center text-upper f-24 f-100 text-gray">
+                        <Text className="text-center text-upper f-16 f-100 text-gray">
                           ${property?.salePrice}
                         </Text>
                       </Flex>
-                      {/* <Flex vertical>
-                        <Text className="text-center text-upper f-24 f-bold text-white">
-                          Agent Listing
-                        </Text>
-                        <Text className="text-center text-upper f-24 f-100 text-gray">
-                          Abraham
-                        </Text>
-                      </Flex> */}
-                      <Flex vertical>
+                      <Flex direction="vertical">
                         <button className="let-talk-btn">View Property</button>
                       </Flex>
                     </Flex>
                   </div>
-
                   <div className="info">
-                    <Flex justify={"end"} align={"center"}>
-                      <Flex>
+                    <Flex justify="end" align="center">
+                      <Flex align="center">
                         <IoLocationOutline color="white" size={20} />
                         <Text
                           className="f-14 f-bold text-white"
-                          style={{ textAlign: "right" }}
+                          style={{ textAlign: "right", marginLeft: 8 }}
                         >
-                          {property?.addressLine1} {property?.addressLine2}{" "}
-                          <br />
+                          {property?.addressLine1} {property?.addressLine2} <br />
                           <IoPricetagOutline size={20} /> ${property?.salePrice}
                         </Text>
                       </Flex>
@@ -126,7 +117,7 @@ function SearchCommunity() {
           {data?.properties.length === 0 ? null : (
             <Flex justify={"center"} align="center" className="my-4">
               <Pagination
-                defaultCurrent={1}
+                current={currentPage}
                 total={data?.totalCount}
                 pageSize={itemsPerPage}
                 onChange={handlePageChange}
