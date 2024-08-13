9904ff649ccc1f141afe500bbecafbe9ad3151a9
A-hasan-code <alihasan12soft786@gmail.com>
2024-08-14 01:15:32 +0500

    coomunities loader

--- src/pages/Home/AllCommunicaties.jsx
+++ src/pages/Home/AllCommunicaties.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Typography, Flex, Row, Col ,Spin} from "antd";
+import { Typography, Flex, Row, Col, Spin } from "antd";
 import Slider from "react-slick";
 import Florida from "../../assets/florida.png";
 import useCommunities from "../../hooks/useCommunities";
@@ -10,7 +10,9 @@ import BoatImage from "../../assets/boatowner.png";
 const { Title, Text, Paragraph } = Typography;
 function Allcommunities() {
   const { data, isLoading } = useCommunities(20, 1);
-   const sortingArr = [...(data?.filters ?? [])].sort((a, b) => a?.name?.localeCompare(b?.name));
+  const sortingArr = [...(data?.filters ?? [])].sort((a, b) =>
+    a?.name?.localeCompare(b?.name)
+  );
   const navigate = useNavigate();
 
   const settings = {
@@ -24,25 +26,25 @@ function Allcommunities() {
         settings: {
           slidesToShow: 4,
           slidesToScroll: 2,
-          infinite: true,
-        },
+          infinite: true
+        }
       },
       {
         breakpoint: 768, // Adjust as per your requirement
         settings: {
           slidesToShow: 2,
           slidesToScroll: 1,
-          initialSlide: 1,
-        },
+          initialSlide: 1
+        }
       },
       {
         breakpoint: 425, // Adjust as per your requirement
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
-          initialSlide: 1,
-        },
-      },
+          initialSlide: 1
+        }
+      }
       // Add more responsive configurations as needed
     ],
     swipeToSlide: true,
@@ -50,13 +52,18 @@ function Allcommunities() {
       console.log(
         `Slider Changed to: ${index + 1}, background: #222; color: #bada55`
       );
-    },
+    }
   };
   return (
     <>
       <BackgroundImage Image={BoatImage}>
         <Title
-          style={{ color: "white", lineHeight: "46px", letterSpacing: "2px", textAlign: "center" }}
+          style={{
+            color: "white",
+            lineHeight: "46px",
+            letterSpacing: "2px",
+            textAlign: "center"
+          }}
           className="text-upper f-50 f-100"
         >
           All Communities
@@ -67,51 +74,54 @@ function Allcommunities() {
           Featured Communties
         </Title>
         <Row className="px-4">
-          {sortingArr.map((community, index) => (
-            <Col lg={6} md={12} sm={24} key={index} className="">
-              <div className="displayy-teamimg-center show-btn-community-home ">
-                <img
-                  src={community?.photo}
-                  width="100%"
-                  className="img-op communities-grid "
-                />
-                {isLoading ? (
-        <Flex justify={"center"} xs={24} sm={12} md={8} lg={6}>
-          <Spin size="large" />
-        </Flex>
-      ) : (<div
-                  style={{
-                    position: "absolute",
-                    top: "50%",
-                    left: "0%",
-                    width: "100%",
-                  }}
-                >
-                  <Flex
-                    justify={"center"}
-                    align={"center"}
-                    style={{ width: "95%" }}
-                    vertical
+          {isLoading ? (
+            <Flex justify={"center"} xs={24} sm={12} md={8} lg={6}>
+              <Spin size="large" />
+            </Flex>
+          ) : (
+            sortingArr.map((community, index) => (
+              <Col lg={6} md={12} sm={24} key={index} className="">
+                <div className="displayy-teamimg-center show-btn-community-home">
+                  <img
+                    src={community?.photo}
+                    width="100%"
+                    className="img-op communities-grid"
+                  />
+                  <div
+                    style={{
+                      position: "absolute",
+                      top: "50%",
+                      left: "0%",
+                      width: "100%"
+                    }}
                   >
-                    <Text
-                      className="text-upper text-white f-100"
-                      style={{ fontSize: "20px" }}
+                    <Flex
+                      justify={"center"}
+                      align={"center"}
+                      style={{ width: "95%" }}
+                      vertical
                     >
-                      {community?.name}
-                    </Text>
+                      <Text
+                        className="text-upper text-white f-100"
+                        style={{ fontSize: "20px" }}
+                      >
+                        {community?.name}
+                      </Text>
 
-                    <button
-                      className="button-view1"
-                      onClick={() => navigate(`/community/${community?._id}`)}
-                    >
-                      View
-                    </button>
-                  </Flex>
-                </div>)}
-              </div>
-            </Col>
-          ))}
+                      <button
+                        className="button-view1"
+                        onClick={() => navigate(`/community/${community?._id}`)}
+                      >
+                        View
+                      </button>
+                    </Flex>
+                  </div>
+                </div>
+              </Col>
+            ))
+          )}
         </Row>
+
         {/* <Flex
         justify={"center"}
         align={"center"}
