6afac0ddc0087936594bf63eaacf7454c18ee13d
A-hasan-code <alihasan12soft786@gmail.com>
2024-08-25 21:35:53 +0500

    axios

--- src/api/Axios.jsx
+++ src/api/Axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-export const api_base_URL = "https://florida-lux-api.onrender.com/v1/";
-
+export const api_base_URL = "http://localhost:3002/v1/";
+//export const api_base_URL = "https://florida-lux-api.onrender.com/v1/";
 //export const api_base_URL = "https://a032-182-177-42-127.ngrok-free.app/v1/";
 // export const api_base_URL = "https://florida-lux-back.waveio.site/v1/";
 //export const api_base_URL = "https://florida-lux-nodejs.onrender.com/v1/";
@@ -18,7 +18,6 @@ customAxios.interceptors.response.use(
   (error) => {
     if (error.response.status === 401) {
       localStorage.removeItem("token");
-
       window.location.href = "/admin/login";
     } else if (error.response.status === 400) {
       console.log("404");
