6a55e22d4ecf1625d3d08112eed96f08e81ef2c9
Abraham Peter <abraham.peter.hash@gmail.com>
2024-10-30 21:33:25 +0500

    new build with railways

--- src/pages/AgentBlog/AgentBlog.jsx
+++ src/pages/AgentBlog/AgentBlog.jsx
@@ -37,6 +37,7 @@ const Flipbook = ({ pages, flipbookRef }) => {
       }}
     >
       {pages.map((page, index) => (
+        
         <div key={index} className="page" style={{ padding: "10px" }}>
           <img
             src={page}
