8ade7d6237b219aae5c026c20fbda7b5b6d6ff48
TashfeenHayat <tashfeensikanderhayat@gmail.com>
2024-12-30 23:50:08 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   root: ".", // Ensure Vite looks in the right directory for index.html
+   base: "/",
   plugins: [react()],
   optimizeDeps: {
     exclude: ["jquery"], // Exclude jQuery from optimization
