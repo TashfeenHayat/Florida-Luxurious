d2f7cc343fb59638c507871b760582d9fba65138
TashfeenHayat <tashfeensikanderhayat@gmail.com>
2025-01-13 23:07:58 +0500

    Update useAgents.jsx

--- src/hooks/useAgents.jsx
+++ src/hooks/useAgents.jsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getAgents } from "../api/Agents";
 function useAgents(limit, page) {
-  console.log(page, limit, "checking");
+  //console.log(page, limit, "checking");
   const dispatch = useDispatch();
   const { isLoading, data } = useSelector((s) => s.getAgentsReducer);
   useEffect(() => {
