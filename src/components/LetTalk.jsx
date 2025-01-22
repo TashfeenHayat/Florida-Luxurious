fa212560b868ecabdd487a73ade7c8f200adc289
TashfeenHayat <tashfeensikanderhayat@gmail.com>
2025-01-22 06:51:12 +0500

    Update LetTalk.jsx

--- src/components/LetTalk.jsx
+++ src/components/LetTalk.jsx
@@ -10,7 +10,8 @@ function LetTalk() {
     <div className="background-video-container" style={{ height: "70vh" }}>
       <video autoPlay muted loop className="background-video">
         <source
-          src="https://firebasestorage.googleapis.com/v0/b/floridaluxrious.appspot.com/o/talk.mp4?alt=media&token=859b58a0-8029-4b2c-8059-69a25f3ccd95"
+          src="https://firebasestorage.googleapis.com/v0/b/florida-lux-e66c2.firebasestorage.app/o/contactus.mp4?alt=media&token=77547d74-277e-4285-9725-561eb49e27d3"
+          //src="https://firebasestorage.googleapis.com/v0/b/floridaluxrious.appspot.com/o/talk.mp4?alt=media&token=859b58a0-8029-4b2c-8059-69a25f3ccd95"
           type="video/mp4"
         />
       </video>
