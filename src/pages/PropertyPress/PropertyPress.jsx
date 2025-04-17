7bd863f7085b31a5062f6b9d9435ab7875649ce6
TashfeenHayat <tashfeensikanderhayat@gmail.com>
2025-04-18 03:03:48 +0500

    Update PropertyPress.jsx

--- src/pages/PropertyPress/PropertyPress.jsx
+++ src/pages/PropertyPress/PropertyPress.jsx
@@ -1,22 +1,174 @@
-import React from "react";
-import { Typography, Row, Col, Image, Spin } from "antd";
+import React, { useEffect, useRef, useState } from "react";
+import { Typography, Row, Col, Spin, Button, Alert } from "antd";
 import { Container } from "react-bootstrap";
-import usePress from "../../hooks/usePress";
-import { useNavigate } from "react-router-dom";
-import { useMediaQuery } from "react-responsive";
+import usePressDetail from "../../hooks/usePressDetail";
+import { useParams } from "react-router-dom";
+import { decode } from "html-entities";
+import HTMLFlipBook from "react-pageflip";
+import * as pdfjsLib from "pdfjs-dist/build/pdf";
+import Flogo from "../../assets/flipbook.png";
+const { Title } = Typography;
 
-const { Title, Text } = Typography;
+pdfjsLib.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.worker.min.js`;
 
-function PropertyPress() {
-  const { data, isLoading, error } = usePress();
-  // console.log("press", data);
-  // Check if data is an object with a posts array
-  const pressData = data?.posts || [];
+const Flipbook = React.forwardRef(
+  ({ pages, onPageChange, flipbookRef }, ref) => {
+    const isMobile = window.innerWidth < 600;
+    const flipbookWidth = isMobile ? window.innerWidth * 0.9 : 500;
+    const flipbookHeight = isMobile ? flipbookWidth * 1.4 : 700;
+    return (
+      <HTMLFlipBook
+        width={flipbookWidth}
+        height={flipbookHeight}
+        size="stretch"
+        minWidth={300}
+        maxWidth={600}
+        minHeight={400}
+        maxHeight={500}
+        drawShadow={true}
+        flippingTime={1000}
+        useMouseEvents={true}
+        ref={flipbookRef}
+        style={{
+          margin: "0 auto",
+          maxWidth: "100%",
+        }}
+        onFlip={onPageChange}
+      >
+        {pages?.length > 0 && (
+          <div
+            className="pageblank"
+            key="blank"
+            style={{
+              display: "none",
+              width: "100%",
+              height: "auto",
+            }}
+          >
+            <img
+              src={pages[0] || ""}
+              alt="Cover Page"
+              style={{
+                width: "100%",
+                height: "auto",
+                borderRadius: "20px",
+              }}
+            />
+          </div>
+        )}
 
-  const navigate = useNavigate();
+        {pages?.length > 0 && (
+          <div className="page cover" key="cover">
+            <img
+              src={pages[0] || ""}
+              alt="Cover Page"
+              style={{
+                width: "100%",
+                height: "auto",
+                borderRadius: "20px",
+              }}
+            />
+          </div>
+        )}
 
-  // Use a media query to detect if the screen width is less than or equal to 768px (mobile/tablet)
-  const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
+        {pages.slice(1)?.map((page, index) => (
+          <div className="page" key={index + 1}>
+            <img
+              src={page}
+              alt={`Page ${index + 2}`}
+              style={{
+                width: "100%",
+                height: "auto",
+                borderRadius: "20px",
+              }}
+            />
+          </div>
+        ))}
+      </HTMLFlipBook>
+    );
+  }
+);
+
+function PropertyPressDetail() {
+  const { id } = useParams();
+  const { data, isLoading, isError } = usePressDetail(id);
+  const refHtml = useRef(null);
+  const flipbookRef = useRef(null);
+
+  const [pages, setPages] = useState([]);
+  const [loadingPages, setLoadingPages] = useState(true);
+  const [isCoverPage, setIsCoverPage] = useState(true);
+  useEffect(() => {
+    if (refHtml.current && data?.content) {
+      const decodedContent = decode(data.content);
+      refHtml.current.innerHTML = decodedContent;
+
+      const images = refHtml.current.querySelectorAll("img");
+      images.forEach((img) => {
+        img.style.maxWidth = "100%";
+        img.style.height = "auto";
+      });
+    }
+  }, [data?.content]);
+
+  useEffect(() => {
+    if (!data?.file) {
+      setLoadingPages(false);
+    } else {
+      setLoadingPages(true);
+      const url = data?.file;
+      const loadingTask = pdfjsLib.getDocument(url);
+      loadingTask.promise.then((pdf) => {
+        const totalPages = pdf.numPages;
+        const pageImages = [];
+
+        const loadPage = async (pageNumber) => {
+          const page = await pdf.getPage(pageNumber);
+          const scale = 1.5;
+          const viewport = page.getViewport({ scale });
+
+          const canvas = document.createElement("canvas");
+          const context = canvas.getContext("2d");
+          canvas.width = viewport.width;
+          canvas.height = viewport.height;
+
+          await page.render({ canvasContext: context, viewport }).promise;
+
+          const imgData = canvas.toDataURL("image/png");
+          if (imgData) {
+            pageImages.push(imgData);
+          }
+          console.log(pages); // Add this before returning Flipbook component
+
+          if (pageImages.length === totalPages) {
+            setPages([...pageImages]);
+            setLoadingPages(false);
+          }
+        };
+
+        for (let pageNumber = 1; pageNumber <= totalPages; pageNumber++) {
+          loadPage(pageNumber);
+        }
+      });
+    }
+  }, [data?.file]);
+
+  const handlePrevPage = () => {
+    if (flipbookRef.current) {
+      flipbookRef.current.pageFlip().flipPrev();
+    }
+  };
+
+  const handleNextPage = () => {
+    if (flipbookRef.current) {
+      if (isCoverPage) {
+        setIsCoverPage(false);
+        flipbookRef.current.pageFlip().flipNext();
+      } else {
+        flipbookRef.current.pageFlip().flipNext();
+      }
+    }
+  };
 
   return (
     <>
@@ -27,101 +179,95 @@ function PropertyPress() {
               display: "flex",
               justifyContent: "center",
               alignItems: "center",
+              textAlign: "center",
+              padding: "20px",
             }}
           >
-            <Title className="text-upper text-white f-50 f-100">
-              Our Market
+            <Title
+              className="text-upper text-white f-50 f-100 press-title"
+              style={{ fontSize: "clamp(24px, 5vw, 50px)" }}
+            >
+              {data?.title}
             </Title>
           </div>
         </div>
       </div>
-      <Container className="py-4">
-        <Row gutter={[30, 30]}>
-          {isLoading ? (
-            <Col span={24}>
-              <div
-                style={{
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  width: "100%",
-                }}
-              >
+      {isLoading ? (
+        <Row
+          style={{
+            minHeight: "50vh",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Col>
+            <Spin size="large" />
+          </Col>
+        </Row>
+      ) : isError ? (
+        <Row style={{ minHeight: "50vh" }}>
+          <Col>
+            <Alert message="Failed to load blog content" type="error" />
+          </Col>
+        </Row>
+      ) : (
+        <Container style={{ padding: "15px", flex: "1" }} justify="center">
+          <div ref={refHtml} className="press-market" />
+          {loadingPages ? (
+            <Row
+              style={{
+                minHeight: "50vh",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <Col>
                 <Spin size="large" />
-              </div>
-            </Col>
-          ) : error ? (
-            <Col span={24}>
-              <div
-                style={{
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  width: "100%",
-                }}
-              >
-                <Text type="danger">Failed to load data.</Text>
-              </div>
-            </Col>
-          ) : pressData?.length > 0 ? (
-            pressData?.map((item, index) => (
-              <React.Fragment key={index}>
-                <Col xs={24} sm={24} md={12} lg={12}>
-                  <Image
-                    src={item?.cover}
-                    preview={false}
-                    style={{ width: "100%" }}
-                  />
-                </Col>
-                <Col xs={24} sm={24} md={12} lg={12}>
-                  <div>
-                    <Title className="text-upper f-30 f-100">
-                      {item?.title}
-                    </Title>
-                    <Text>{item?.date}</Text>
-                  </div>
-                  <div
+              </Col>
+            </Row>
+          ) : (
+            pages?.length > 1 && (
+              <>
+                <Flipbook pages={pages} flipbookRef={flipbookRef} />
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    marginTop: "20px",
+                    width: "100%",
+                  }}
+                >
+                  <Button
+                    className="button-preview"
+                    onClick={handlePrevPage}
                     style={{
-                      display: "flex",
-                      justifyContent: isMobile ? "center" : "end", // Center button on mobile
-                      alignItems: "center",
-                      marginTop: 24,
+                      marginRight: "10px",
+                      padding: "10px 20px",
+                      
                     }}
                   >
-                    <button
-                      className="button-secondary text-upper"
-                      style={{
-                        width: "100%",
-                        maxWidth: "200px",
-                        background: "black",
-                        color: "white",
-                      }}
-                      onClick={() => navigate(`/ourmarket/${item._id}`)}
-                    >
-                      Read More
-                    </button>
-                  </div>
-                </Col>
-              </React.Fragment>
-            ))
-          ) : (
-            <Col span={24}>
-              <div
-                style={{
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  width: "100%",
-                }}
-              >
-                <Text>No data available.</Text>
-              </div>
-            </Col>
+                    Previous Page
+                  </Button>
+                  <Button
+                    className="button-next"
+                    onClick={handleNextPage}
+                    style={{
+                      padding: "10px 20px",
+                      
+                    }}
+                  >
+                    Next Page
+                  </Button>
+                </div>
+              </>
+            )
           )}
-        </Row>
-      </Container>
+        </Container>
+      )}
     </>
   );
 }
 
-export default PropertyPress;
+export default PropertyPressDetail;
