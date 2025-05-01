2ed5da4a5c20acdcfaf353f6c077b77c939abd4c
TashfeenHayat <tashfeensikanderhayat@gmail.com>
2025-05-01 22:41:57 +0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./redux/Store";
@@ -8,10 +7,28 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import loading from "./assets/logoicon1.png";
+const App = lazy(() => import("./App.jsx"));
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Suspense
+          fallback={
+            <div id="loading">
+              <h1 style={{ textTransform: "uppercase" }}>
+                Florida Luxurious Properties
+              </h1>
+              <br />
+
+              <img src={loading} style={{ width: "20%" }} alt="Loading" />
+            </div>
+          }
+        >
+          <App />
+        </Suspense>
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
 );
